071badbeae83e157588188b313cfaa5f07cf429c
Graham Kent <gskent7@gmail.com>
2021-08-23 14:46:03 -0700

    updated Logout and Header components with resonsive font-size

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,14 +6,13 @@ import '../Styles/Header.css';
 
 const Header = (props) => {
     return (
-            <Navbar expand="sm" variant="dark">
-                <Navbar.Brand>
+            <Navbar expand="md" variant="dark">
+                <Navbar.Brand className="text-center">
                     <Link to='/' className="title font-weight-bold text-wrap">SURVIVOR SELECTOR</Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbar-menu"/>
                 <Navbar.Collapse id="navbar-menu">
                     <Nav className="ml-auto">
-                        <Link to='/' className="header-link">Home</Link>
                         {(props.token === undefined)
                             ? <Link to='/login' className="header-link">Login</Link>
                             :   <>
